78e3c6453eb1ffc2c4853ee95f72506ed1aebf05
Sharmila <newkobytes@gmail.com>
2022-09-11 11:02:29 +0530

    Maven codes

---
+++ src/test/java/TestCases/HomeTest.java
@@ -0,0 +1,12 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class HomeTest extends BaseClass {
+
+	@Test
+	public void Test1()
+	{
+		System.out.println("Inside HomeTest");
+	}
+}
---
+++ src/test/java/TestCases/SimpliLearnLogin.java
@@ -0,0 +1,61 @@
+package TestCases;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import Pages.LoginPage;
+
+	
+public class SimpliLearnLogin extends BaseClass{
+	
+	
+	@Test
+	public void Test1() {
+		
+		LoginPage lp = new LoginPage(driver);
+		lp.Login("abc@xyz.com", "Abcd12345"); //Test1 method for positive tc and Test2 for negative tc
+		
+		//Step6: Validate the error message on screen
+		WebElement Error = driver.findElement(By.id("msg_box"));
+		
+		String ActError = Error.getText();
+		String ExpError = "The email or password you have entered is invalid.";
+//Assert.assertFalse(Error.isDisplayed());
+Assert.assertTrue(Error.isDisplayed());
+Assert.assertEquals(ActError, ExpError);
+		/*if(ActError.equals(ExpError)) {
+			System.out.println("TC PAssed");
+		}else {
+			System.out.println("Tc Failed");
+		}	*/ //when if stmnt is used though we give ExpError value as something else we will get the output as TC Failed but the 
+		//Testng is still passed, so to prevent this we need to make TC fail if the condition is failed using assertions
+		// we need to use Assertions in testng instead of if else condition
+	}
+	
+	@Test
+	@Parameters({"uname","pwd"})
+	public void Test2(String UN , String PW) {
+		LoginPage lp = new LoginPage(driver);
+		lp.Login(UN, PW);
+	}	
+		@Test
+		public void Test3() {
+			
+			String UserName = sheet.getRow(1).getCell(0).getStringCellValue();
+	        String Password = sheet.getRow(1).getCell(1).getStringCellValue();
+			LoginPage lp = new LoginPage(driver);
+			lp.Login(UserName, Password);
+			
+		
+	
+	}
+}
+
+
